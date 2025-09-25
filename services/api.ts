c36c31e58be6aa971d64451807632c2861775e23
rkvtsn <rkvtsn@gmail.com>
2025-09-24 22:12:58 -0700

    bug: log4

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,8 @@
 // @todo contract type - openapi or any other
 export const API = {
-  CATEGORIES: "/api/categories",
-  CATEGORIES_PRODUCTS: "/api/categories/products",
-  PRODUCTS: "/api/products",
-  PRODUCTS_SEARCH: "/api/products/search",
-  FEATURES: "/api/features",
+  CATEGORIES: "/categories",
+  CATEGORIES_PRODUCTS: "/categories/products",
+  PRODUCTS: "/products",
+  PRODUCTS_SEARCH: "/products/search",
+  FEATURES: "/features",
 } as const;
